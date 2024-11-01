19bf1359dbd464e335caa59b0ae60ec5e7c57ccf
Elijah Kim <reachelijah@gmail.com>
2024-11-01 09:53:32 -0700

    tiptap bullet, color, bold extensions

--- src/app/components/outlineEditor.tsx
+++ src/app/components/outlineEditor.tsx
@@ -1,11 +1,33 @@
 "use client";
 import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
+import Bold from "@tiptap/extension-bold";
+import { Color } from "@tiptap/extension-color";
+import BulletList from "@tiptap/extension-bullet-list";
+import Document from "@tiptap/extension-document";
+import ListItem from "@tiptap/extension-list-item";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import "./tiptapStyles.css";
+import TextStyle from "@tiptap/extension-text-style";
 
 const OutlineEditor = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      BulletList,
+      ListItem,
+      Bold,
+      TextStyle,
+      Color,
+    ],
+    content: `
+        <ul>
+          <li><span style="color: #958DF1">A list item</span></li>
+          <li>And another one</li>
+        </ul>
+        `,
   });
 
   return <EditorContent editor={editor} />;
