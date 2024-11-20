2f849136da98dc8dd2a22525aa3388835377bbeb
Elijah Kim <reachelijah@gmail.com>
2024-11-19 16:22:13 -0800

    adjust padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import OutlineEditor from "./components/luminaEditor/outlineEditor";
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 items-start w-full max-w-[100em] pt-36">
+      <main className="flex flex-col gap-8 items-start w-full max-w-[100em]">
         <h1 className="text-4xl font-bold text-center sm:text-left">Lumina</h1>
-        <div className="text-md text-center sm:text-left max-w-[80%] text-gray-600">
+        <div className="text-md text-center sm:text-left text-gray-600">
           <p>
             Lumina is a simple and powerful outliner for brainstorming college
             application essays. Lumina pushes you to reflect more deeply and
